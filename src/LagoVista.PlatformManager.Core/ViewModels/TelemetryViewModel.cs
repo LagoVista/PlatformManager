186655f340765f2ebcf7a2835f05410bdf573572
Kevin D. Wolf <kevinw@software-logistics.com>
2017-12-29 06:50:20 -0500

    start framing out telemetry

--- src/LagoVista.PlatformManager.Core/ViewModels/TelemetryViewModel.cs
+++ src/LagoVista.PlatformManager.Core/ViewModels/TelemetryViewModel.cs
@@ -1,11 +1,33 @@
 ﻿using LagoVista.Client.Core.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace LagoVista.PlatformManager.Core.ViewModels
 {
     public class TelemetryViewModel : AppViewModelBase
     {
+
+        public string ViewType { get; set; }
+        public string Id { get; set; }
+
+        public void ShowStatusHistory()
+        {
+            var statusHistoryUri = $"/api/deployment/{ViewType}/{Id}/statushistory";
+        }
+
+        public void ShowCustomEvents()
+        {
+            var telemetry = $"/api/telemetry/customEvents/{ViewType}/{Id}";
+        }
+
+
+        public void ShowExceptions()
+        {
+            var telemetry = $"/api/telemetry/error/{ViewType}/{Id}";
+        }
+
+        public ObservableCollection<LagoVista.IoT.Deployment.Admin.Models.TelemetryReportData> ReportData {get; set;}
     }
 }
